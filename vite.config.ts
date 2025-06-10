b9771e93dc38a6045f9b29f39527739b2b2e1653
Nouval Rizky <nouvalr@gmail.com>
2025-06-10 21:03:16 +0800

    chore: add env for api base url

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,24 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:4000",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+export default defineConfig(({ mode }) => {
+  // Load .env file based on current mode (development, production, etc)
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_BASE_API_URL, // gunakan env di sini
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      },
+      watch: {
+        ignored: ["**/db.json"],
       },
     },
-    watch: {
-      ignored: ["**/db.json"],
-    },
-  },
+  };
 });
